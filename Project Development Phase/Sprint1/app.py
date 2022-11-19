c6a2d3c568730626d86da874ccd3dae986949959
janapreethi <90337510+janapreethi@users.noreply.github.com>
2022-11-19 21:17:24 +0530

    Create app.py

---
+++ Project Development Phase/Sprint1/app.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField, IntegerField
+import ibm_db
+from passlib.hash import sha256_crypt
+from functools import wraps
+
+from sendgrid import *
+
+#creating an app instance
+app = Flask(__name__)
+
+app.secret_key='a'
+conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=824dfd4d-99de-440d-9991-629c01b3832d.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=30119;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=tyt27664;PWD=hrAJhoiC3znla3rh;",'','')
+
+#Index
+@app.route('/')
+def index():
+    return render_template('home.html')
+#Register Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=1, max=25)])
+    email = StringField('Email', [validators.length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+#user register
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        sql1="INSERT INTO users(name, email, username, password) VALUES(?,?,?,?)"
+        stmt1 = ibm_db.prepare(conn, sql1)
+        ibm_db.bind_param(stmt1,1,name)
+        ibm_db.bind_param(stmt1,2,email)
+        ibm_db.bind_param(stmt1,3,username)
+        ibm_db.bind_param(stmt1,4,password)
+        ibm_db.execute(stmt1)
+        #for flash messages taking parameter and the category of message to be flashed
+        flash("You are now registered and can log in", "success")
+        
+        #when registration is successful redirect to home
+        return redirect(url_for('login'))
+    return render_template('register.html', form = form)
+
+
+#User login
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        #Get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        sql1="Select * from users where username = ?"
+        stmt1 = ibm_db.prepare(conn, sql1)
+        ibm_db.bind_param(stmt1,1,username)
+        result=ibm_db.execute(stmt1)
+        d=ibm_db.fetch_assoc(stmt1)
+        if result > 0:
+            #Get the stored hash
+            data = d
+            password = data['PASSWORD']
+
+            #compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                #Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash("you are now logged in","success")
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid Login'
+                return render_template('login.html', error=error)
+            #Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.secret_key = "secret123"
+    #when the debug mode is on, we do not need to restart the server again and again
+    app.run(debug=True)
